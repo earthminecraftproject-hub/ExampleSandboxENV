4e163a71b122ddaffc076feda6d83d61b6377e1b
earthminecraftproject-hub <earth.minecraftproject@gmail.com>
2026-06-19 17:07:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { exec } from "child_process";
 
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 app.post("/run", (req, res) => {
